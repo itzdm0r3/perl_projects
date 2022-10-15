a87a9ef04d65ff2539940f0fa751fd64aec46b6c
Dwayne Moreland <dmoreland84@gmail.com>
2022-10-15 06:25:35 -0400

    wrote user input collection section.

--- WilliamTheGreat.pl
+++ WilliamTheGreat.pl
@@ -25,14 +25,52 @@ $food = "";
 $smell = "";
 $animal = "";
 
+#display an introductory message and format display using carriage returns.
 print "This is the Story of William The Great.\n\n";
-print "Press the Enter key to continue...";
+print "Press the Enter key to continue..."; #prompt user before continuing.
+#set variable equal to standard input and remove any trailing newline.
 chomp($reply = <STDIN>);
 
 ###GIVING THE PLAYER INSTRUCTIONS###
 
-clear_the_screen();
+clear_the_screen(); #call subroutine that clears the screen.
 
 print "To hear this story, you must provide some information.\n\n";
 print "Press Enter key to continue...";
+$reply = <STDIN>;   #prompt user before continuing.
+
+###COLLECTING USER INPUT###
+
+clear_the_screen(); #clear the screen.
+
+print "Enter the name of an old fashioned vehicle.\n\n";
+print ">";  #display input prompt.
+chomp ($vehicle = <STDIN>); #capture the user's answer.
+
+print "What is your favorite type of dessert?\n\n";
+print ">";  #display input prompt.
+chomp ($dessert = <STDIN>); #capture the user's answer.
+
+clear_the_screen(); #clear the screen.
+
+print "What is best with jelly or jam?\n\n";
+print ">";  #display input prompt
+chomp ($food = <STDIN>); #capture the user's answer.
+
+clear_the_screen(); #clear the screen.
+
+print "Name a type of smell that reminds you of your grandmother.\n\n";
+print ">";  #display input prompt.
+chomp ($smell = <STDIN>); #capture the user's answer.
+
+clear_the_screen(); #clear the screen.
+
+print "Name a large wild animal.\n\n";
+print ">"; #display input prompt.
+chomp ($animal = <STDIN>); #capture the user's answer.
+
+clear_the_screen(); #clear the screen.
+
+print "Good answers. Let's continue with the story.\n\n";
+print "Press the Enter key to continue..."; #prompt user before continuing.
 $reply = <STDIN>;
