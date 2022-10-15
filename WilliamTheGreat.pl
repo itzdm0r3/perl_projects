5d0bd510ed8e3f3111f1045ff5bb2b038705deb1
Dwayne Moreland <dmoreland84@gmail.com>
2022-10-15 05:20:31 -0400

    created chapter 2 game script.

---
+++ WilliamTheGreat.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+#The Story of William The Great (WilliamTheGreat.pl)
+
+###DESIGNING THE GAME LOGIC###
+#1. Create and document new Perl script.
+#2. Prep for data collection and introduce the story.
+#3. Provide the user with instructions
+#4. Collect user input
+#5. Begin the story.
+#6. Tell the story.
+#7. End the story.
+#8. Create a subroutine to clear the screen.
